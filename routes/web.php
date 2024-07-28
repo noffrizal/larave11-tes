8563e21a1b212dd085ab48efaf22b3bc03b10907
noffrizalzaim <zaimjr@gmail.com>
2024-07-29 04:28:19 +0700

    update view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,14 +11,18 @@ Route::get('/about', function () {
     return view('about',['title' => 'About Page','name' => 'NOFFRIZAL']);
 });
 
-route::get('/blog', function () {
-    return view('blog',['title' => 'Blog Page', 'posts'=>[
+route::get('/posts', function () {
+    return view('posts',['title' => 'Blog Page', 'posts'=>[
         [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'author' => 'Noffrizal Zaim',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima tempora cum consequuntur officia. Nihil sed fugit consectetur facilis officiis asperiores.'
         ],
         [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Noffrizal Zaim',
             'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit inventore mollitia nisi facilis, ipsa illo molestias eveniet. Distinctio nisi amet tenetur debitis! Fugit laborum tempore quaerat atque corporis, explicabo maxime alias officia autem. Doloribus esse pariatur sunt fuga, nobis sit consequuntur quisquam quas porro doloremque similique dignissimos, iure temporibus non.'
@@ -25,6 +30,37 @@ route::get('/blog', function () {
     ]]);
 });
 
-route::get('/contact', function () {
+Route::get('/posts/{slug}', function ($slug) {
+  $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Noffrizal Zaim',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima tempora cum consequuntur officia. Nihil sed fugit consectetur facilis officiis asperiores.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Noffrizal Zaim',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit inventore mollitia nisi facilis, ipsa illo molestias eveniet. Distinctio nisi amet tenetur debitis! Fugit laborum tempore quaerat atque corporis, explicabo maxime alias officia autem. Doloribus esse pariatur sunt fuga, nobis sit consequuntur quisquam quas porro doloremque similique dignissimos, iure temporibus non.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+
+
+    return view('post', [
+        'title' => 'Single Post',
+        'post' => $post
+    ]);
+
+});
+
+Route::get('/contact', function () {
     return view('contact',['title' => 'Contact Page']);
 });
