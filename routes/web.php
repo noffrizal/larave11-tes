22924c11e8c959769d102720b6290089556000ad
noffrizal <zaimjr@gmail.com>
2024-08-21 10:56:01 +0700

    make PostFactory

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-
-
-
 Route::get('/', function () {
     return view('home',['title' => 'Home Page']);
 });
