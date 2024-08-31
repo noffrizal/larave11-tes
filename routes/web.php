0b2f03c95ca5f7efe693c49a98966f7cab147dda
noffrizalzaim <zaimjr@gmail.com>
2024-08-31 22:20:46 +0700

    Make Post and User Relationship

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -18,14 +19,13 @@ route::get('/posts', function () {
 
 Route::get('/posts/{post:slug}', function (Post $post) {
 
+    return view('post', ['title' => 'Single Post','post' => $post]);
 
+});
 
+Route::get('/authors/{user}', function (User $user) {
 
-
-    return view('post', [
-        'title' => 'Single Post',
-        'post' => $post
-    ]);
+    return view('posts', ['title' => 'Articles by '. $user->name,'posts' => $user->posts]);
 
 });
 
